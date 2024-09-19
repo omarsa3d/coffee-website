23949e3281ee8a58792c867a256b5447fdfef18c
omarsa3d <87918992+omarsa3d@users.noreply.github.com>
2024-09-19 06:08:45 +0300

    customers, footer and header animation
    added the customer and footer sections.
    added small animation on header to have background color only on scrolly > 0

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ let searchIcon = document.querySelector('#search-icon')
 searchIcon.onclick = () => {
     search.classList.toggle('active');
 }
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
