842d2b924385471087ef1d616317ea1b77337737
omarsa3d <87918992+omarsa3d@users.noreply.github.com>
2024-09-24 03:29:34 +0300

    made website responsive, replaced lorem text

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let searchIcon = document.querySelector('#search-icon')
 
 searchIcon.onclick = () => {
     search.classList.toggle('active');
+    navbar.classList.remove('active');
 }
 
 let header = document.querySelector('header');
@@ -10,3 +11,16 @@ let header = document.querySelector('header');
 window.addEventListener('scroll', () => {
     header.classList.toggle('shadow', window.scrollY > 0);
 });
+
+let navbar = document.querySelector('.navbar')
+let menuIcon = document.querySelector('#menu-icon')
+
+menuIcon.onclick = () => {
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    search.classList.remove('active');
+}
